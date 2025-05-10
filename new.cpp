914ed1c2e821dcc189f708fd41f43022aa87bd91
Anshil4080 <yadavanshilkumar@gmail.com>
2025-05-10 20:22:18 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Person
+{
+public:
+    string name;
+    string age;
+    void getinfo()
+    {
+        cout << "the name is " << name << "and age is " << age;
+    }
+    int main()
+        Person P;
+    cout << "enter the name " << endl;
+    cin << name;
+    cout << "enter the age" << endl;
+    p.getinfo();
+}
